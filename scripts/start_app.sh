4b1094e4e97040b842e61595debafa459e24a212
Joshua Dhikusooka <30894937+joshard@users.noreply.github.com>
2024-10-10 15:01:11 +0300

    Update start_app.sh

--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -26,6 +26,5 @@ python manage.py collectstatic --noinput
 # Restart Gunicorn and Nginx services
 echo "Restarting Gunicorn and Nginx services..."
 sudo service gunicorn restart
-sudo service nginx restart
 
 echo "Application started successfully."
