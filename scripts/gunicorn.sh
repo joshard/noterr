e8fa9b3a086b57ee4438188a9db703a8bf968c2b
Joshua Dhikusooka <30894937+joshard@users.noreply.github.com>
2024-10-10 14:56:28 +0300

    Update gunicorn.sh

--- scripts/gunicorn.sh
+++ scripts/gunicorn.sh
@@ -4,7 +4,7 @@
 PROJECT_MAIN_DIR_NAME="noterr"
 
 # Copy gunicorn socket and service files
-sudo cp "/home/ubuntu/$PROJECT_MAIN_DIR_NAME/gunicorn/gunicorn.socket" "/etc/systemd/system/gunicorn.socket"
+
 sudo cp "/home/ubuntu/$PROJECT_MAIN_DIR_NAME/gunicorn/gunicorn.service" "/etc/systemd/system/gunicorn.service"
 
 # Start and enable Gunicorn service
